25c09a721438866516325b00e27f1e1cc93bcf18
dwhelan <dwhelan@dpwhelan.com>
2015-11-17 01:47:39 -0500

    Further refactoring into separate methods

--- lib/proc_matcher.rb
+++ lib/proc_matcher.rb
@@ -8,20 +8,17 @@ module RSpec
         return false if actual.arity   != expected.arity
         return false if actual.lambda? != expected.lambda?
 
-        actual_sexp = actual.dup.to_sexp
-
         return true if expected_sexp.sexp_body == actual_sexp.sexp_body
         return false if expected_sexp.count != actual_sexp.count
 
-        p1_params = expected_sexp[2].sexp_body.to_a
         p2_params = actual_sexp[2].sexp_body.to_a
 
-        p1_params.each_with_index do |param, index|
-          actual_sexp[2][index + 1] = param
-          actual_sexp = actual_sexp.gsub Sexp.new(:lvar, p2_params[index]), Sexp.new(:lvar, param)
+        parameters(expected_sexp).each_with_index do |param, index|
+          self.actual_sexp[2][index + 1] = param
+          self.actual_sexp = actual_sexp.gsub Sexp.new(:lvar, p2_params[index]), Sexp.new(:lvar, param)
         end
 
-        expected_sexp == actual_sexp
+        expected_sexp == self.actual_sexp
       end
 
       description do
@@ -32,12 +29,19 @@ module RSpec
         Support::ObjectFormatter.format(source(object))
       end
 
-      private
+      attr_writer :actual_sexp
+
+      def actual_sexp
+        @actual_sexp ||= actual.dup.to_sexp
+      end
 
       def expected_sexp
         @expected_sexp ||= expected.to_sexp
       end
 
+      def parameters(sexp)
+        sexp[2].sexp_body.to_a
+      end
 
       def source(proc)
         source = proc.to_source
@@ -48,4 +52,6 @@ module RSpec
 end
 
 # TODO: handle procs where source extraction fails
+# TODO: handle proc being passed as a block rather than a parameter
 # TODO: handle bindings
+# TODO: split proc equals logic into its own module

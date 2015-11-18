813372050baf452f314cec84a1e1380ad9322c7b
dwhelan <dwhelan@dpwhelan.com>
2015-11-17 22:52:32 -0500

    Minor refactoring

--- lib/proc_matcher.rb
+++ lib/proc_matcher.rb
@@ -15,7 +15,7 @@ class ProcDescriptor
     return false if other.arity != arity || other.lambda? != lambda? || other.count != count
     return true  if other.sexp_body == sexp_body
 
-    other.sexp == with_parameters_from(other).sexp
+    other.sexp == sexp_with_parameters_from(other)
   end
 
   def to_source
@@ -35,17 +35,17 @@ class ProcDescriptor
 
   private
 
-  def with_parameters_from(other_proc)
+  def sexp_with_parameters_from(other_proc)
     new_sexp = sexp
     other_proc.parameters.each_with_index do |param, index|
       new_sexp = rename_parameter(parameters[index], param, new_sexp)
     end
-    ProcDescriptor.new(proc, new_sexp)
+    new_sexp
   end
 
   def rename_parameter(from, to, sexp)
-    new_sexp = replace_parameter_references(from, to, sexp)
-    replace_parameter_definition(from, to, new_sexp)
+    new_sexp = replace_parameter_definition(from, to, sexp)
+    new_sexp = replace_parameter_references(from, to, new_sexp)
     new_sexp
   end
 
@@ -54,8 +54,10 @@ class ProcDescriptor
   end
 
   def replace_parameter_definition(from, to, sexp)
+    new_sexp = sexp.clone
     index = parameters.find_index { |parameter| parameter == from }
-    sexp[2][index + 1] = to
+    new_sexp[2][index + 1] = to
+    new_sexp
   end
 end
 
